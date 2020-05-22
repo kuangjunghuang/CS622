0347c4df9edf53b5bb8ccd93deae5857b75f41c2
kuangjunghuang <kuangjunghuang@yahoo.com>
2020-05-22 19:59:37 -0400

    adding batch files

---
+++ KnowlaGrade1/src/build.bat
@@ -0,0 +1,1 @@
+javac fragments/*.java fragments_test/*.java
--- KnowlaGrade1/src/fragments_test/FragmentDisplayExample.java
+++ KnowlaGrade1/src/fragments_test/FragmentDisplayExample.java
@@ -9,16 +9,16 @@ import fragments.HeadingFragment;
 import fragments.NonHeadingFragment;
 
 class FragmentDisplayExample {
-	
+
 	public static ArrayList<Fragment> theFragments = new ArrayList<Fragment>();
-	
+
 	public static void main(String[] args) { // ?? temp
-		
+
 		System.out.println("=========FragmentDisplayExample==========");
 		displayFragmentHierarchyExample();
 	}
 
-	static void displayFragmentHierarchyExample() {  
+	static void displayFragmentHierarchyExample() {
 	/*
 	 * Postcondition 1 (theFragments): theFragments consists of every Fragment created
 	 * Post2 (Various): Various NonHeadingFragment objects exists within HeadingFragment objects
@@ -26,10 +26,10 @@ class FragmentDisplayExample {
 	 * Post3 (Display): As for displayAll() for "My Expenses Heading"
 	 * Post4 (Counts): As for displayTypeCount()
 	 */
-				
+
 		// displayAll() tested on nontrivial example
-		
-		/* myExpensesHeading denotes the following 
+
+		/* myExpensesHeading denotes the following
 		 My Expenses Heading
 		 Taxes Heading
 		   	Estate taxes
@@ -41,46 +41,46 @@ class FragmentDisplayExample {
 		 */
 
 		// Post1 (theFragments) AND Post2 (Various):
-		
+
 		HeadingFragment myExpensesHeading = new HeadingFragment("My Expenses Heading");
 		theFragments.add(myExpensesHeading); // for Post1
-		 
+
 		NonHeadingFragment estateTaxes = new NonHeadingFragment("Estate taxes");
 		theFragments.add(estateTaxes); // for Post1
 		NonHeadingFragment incomeTaxes = new NonHeadingFragment("Income taxes");
 		theFragments.add(incomeTaxes); // for Post1
 		HeadingFragment taxesHeading = new HeadingFragment("Taxes Heading");
 		theFragments.add(taxesHeading); // for Post1
-		taxesHeading.add(estateTaxes);
-		taxesHeading.add(incomeTaxes);
-		
+		taxesHeading.addFragment(estateTaxes);
+		taxesHeading.addFragment(incomeTaxes);
+
 		NonHeadingFragment mortgageForHousing = new NonHeadingFragment("Mortgage for housing");
 		theFragments.add(mortgageForHousing); // for Post1
 		NonHeadingFragment maintenanceForHousing = new NonHeadingFragment("Maintenance for housing");
 		theFragments.add(maintenanceForHousing); // for Post1
 		HeadingFragment housingHeading = new HeadingFragment("Housing Heading");
 		theFragments.add(housingHeading); // for Post1
-		housingHeading.add(mortgageForHousing);
-		housingHeading.add(maintenanceForHousing);		
+		housingHeading.addFragment(mortgageForHousing);
+		housingHeading.addFragment(maintenanceForHousing);
 
 		NonHeadingFragment food = new NonHeadingFragment("Food");
 		theFragments.add(food); // for Post1
-		
-		myExpensesHeading.add(taxesHeading);
-		myExpensesHeading.add(housingHeading);
-		myExpensesHeading.add(food);
-		
+
+		myExpensesHeading.addFragment(taxesHeading);
+		myExpensesHeading.addFragment(housingHeading);
+		myExpensesHeading.addFragment(food);
+
 		// Post3 (Display):
 		System.out.println("========= Should display 8 items ==========");
-		myExpensesHeading.displayAll();		
+		myExpensesHeading.displayAll();
 		// Post4 (Counts)
 		displayTypeCount();
 	}
-	
+
 	static void displayTypeCount() {
 	// Postcondition: The counts of heading- and non-heading type fragments in theFragments
 	// are on the console
-		
+
 	    int headingCount = 0, nonHeadingCount = 0;
 		for (Fragment fragment : theFragments) {
 	        if (fragment.getType() == "Heading fragment") {

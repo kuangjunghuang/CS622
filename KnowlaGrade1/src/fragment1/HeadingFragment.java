09e5791a0d7b06ab6777039a9b50286fcaa6e736
kuangjunghuang <40582557+kuangjunghuang@users.noreply.github.com>
2021-01-29 19:50:13 -0500

    Update HeadingFragment.java

--- KnowlaGrade1/src/fragment1/HeadingFragment.java
+++ KnowlaGrade1/src/fragment1/HeadingFragment.java
@@ -1,1 +1,64 @@
+package fragments;
+
+//To represent a piece of a passage that's a heading
+
+import java.util.ArrayList;
+
+public class HeadingFragment extends Fragment {
+
+	private ArrayList<Fragment> theSubFragments = new ArrayList<Fragment>();
+
+	public HeadingFragment() {
+		super();
+	}
+
+	public HeadingFragment(String aString) {
+		super(aString);
+	}
+
+	public void addFragment(Fragment aFragment) {
+		theSubFragments.add(aFragment);
+	}
+
+	public void display() {
+		System.out.println(text);
+	}
+
+	public void displayAll() throws NullFragmentException {
+	// Postcondition: the fragments for which this is a heading are on the console
+
+		display();  // the heading itself
+
+		// display sub-headings and their sub-headings
+		for(Fragment fragment: theSubFragments) {
+			if (fragment instanceof HeadingFragment) {
+				((HeadingFragment)fragment).displayAll();
+			}
+			else {
+				if (fragment != null) {
+					fragment.display();
+				} else {
+					throw new NullFragmentException("Fragment is Null");
+				}
+			}
+		}
+	}
+
+	public void setText(String someText) {
+		text = someText.substring(0, someText.length());
+	}
+
+	public ArrayList<Fragment> getSubFragments() {
+		return theSubFragments;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public String getType() {
+		return "Heading fragment";
+	}
+
+}
 
